f188890de98c8391b510d0ad88e8910103552a56
Darren Tebo <darrentebo83@outlook.com>
2020-12-12 13:37:50 -0500

    built zoo repository

---
+++ javazoos/src/main/java/com/lambdaschool/javazoos/repositories/ZooRepository.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.javazoos.repositories;
+
+import com.lambdaschool.javazoos.models.Zoo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ZooRepository extends CrudRepository<Zoo, Long> {
+}
